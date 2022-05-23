959886d3e2f07fe4256922ace6875b081a24f7bc
Timotius Pamungkas (BFI) <timotius.pamungkas@bfi.co.id>
2022-05-23 11:36:49 +0700

    Change unit test

--- src/calculator_test.py
+++ src/calculator_test.py
@@ -1,23 +1,23 @@
 import unittest
-from src.calculator import Calculator
+from calculator import Calculator
 
 class CalculatorTest(unittest.TestCase):
     
     #setUp method is overridden from the parent class TestCase
     def setUp(self):
-        self.calculator = Calculator(125, 5)
+        self.calculator = Calculator(200, 4)
 
     def test_addition(self):
-        self.assertEqual(self.calculator.addition(), 130)
+        self.assertEqual(self.calculator.addition(), 204)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtraction(), 120)
+        self.assertEqual(self.calculator.subtraction(), 196)
 
     def test_multiplication(self):
-        self.assertEqual(self.calculator.multiplication(), 625)
+        self.assertEqual(self.calculator.multiplication(), 800)
 
     def test_division(self):
-        self.assertEqual(self.calculator.division(), 25)
+        self.assertEqual(self.calculator.division(), 500)
 
 if __name__ == "__main__":
     unittest.main()
